a0613cf9f095309e2ee814ddcf2f07985d1098e5
Ailin Pytel <ailin.o.pytel@gmail.com>
2022-01-03 11:40:45 -0300

    prueba export

---
+++ src/components/navBar/navBar.js
@@ -0,0 +1,15 @@
+
+export const navBar = () => {
+    return (
+    <nav className="navBar">
+        <div> 
+            <h1>Hola</h1>
+        </div>
+        <div>
+            <h2>Todos</h2>
+            <h2>Remeras</h2>
+            <h2>Gorras</h2>
+        </div>
+    </nav>
+    )
+}
